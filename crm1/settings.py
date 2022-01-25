68347b288375ec02bf2dbafe4539c4bbd5dfc28c
unknown <aryanit2001@gmail.com>
2022-01-25 15:19:44 +0530

    first commit

--- crm1/settings.py
+++ crm1/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'aryanit2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'Nitin$123'
+EMAIL_HOST_PASSWORD = 'Nitin$123' 
